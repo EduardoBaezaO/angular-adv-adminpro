96f29dc9a8da4c10be4fa9e029e54f2eb2eebeca
Eduardo Baeza O <ebaezao@gmail.com>
2021-08-27 16:18:45 -0400

    Fin sección 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { PagesRoutingModule } from './pages/pages.routing';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { AuthRoutingModule } from './auth/auth-routing';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
 
   //Comentario que indica donde estan las rutas del sistema 20210821EJBO
   // path: './dashboard' PagesRouting
   // path: './auth' AuthRouting
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent }
 ];
---
+++ src/app/servicios/sidebar.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  public menu: any[] = [
+    {
+      titulo: 'DashBoard',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Main', url: '/'},
+        { titulo: 'Progress Bar', url: 'progress'},
+        { titulo: 'Graficas', url: 'grafica1'},
+      ]
+    }
+  ];
+
+  constructor() { }
+}
